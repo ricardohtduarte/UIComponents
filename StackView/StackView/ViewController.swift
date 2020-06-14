3a4119c9af39da89872d04452a1ddf7aa99afbf6
ricardohtduarte <ricardo.duarte@mindera.com>
2020-06-14 21:53:13 +0100

    Calculator using stack view

---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Ricardo Duarte on 14/06/2020.
+//  Copyright © 2020 Ricardo Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
